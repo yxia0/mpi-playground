0bcb6e521899548e8003bf8a1202c9fae573e379
YOUNING XIA <youning.xia@gmail.com>
2023-02-02 18:58:32 +0000

    Add timer to ring

--- src/ring.c
+++ src/ring.c
@@ -25,6 +25,10 @@ int main(void){
     MPI_Comm_rank(comm, &rank);
     MPI_Comm_size(comm, &size);
 
+    double tstart, tstop;
+    MPI_Barrier(MPI_COMM_WORLD);
+    tstart = MPI_Wtime();
+
     printf("Hello from rank %d \n", rank);
 
     sendest = (rank+1) % size;
@@ -38,18 +42,28 @@ int main(void){
         
         printf("Step %d: Process %d send value %d to its neighbour \n", i, rank, curr_val);    
         MPI_Issend(&curr_val, 1, MPI_INT, sendest, 0, comm, &send_request);
-        MPI_Recv(&recv_val, 1, MPI_INT, recvdest, 0, comm, MPI_STATUS_IGNORE);
+        
+        MPI_Barrier(MPI_COMM_WORLD);
 
+        MPI_Recv(&recv_val, 1, MPI_INT, recvdest, 0, comm, MPI_STATUS_IGNORE);
         MPI_Wait(&send_request, &send_status);
+
+        MPI_Barrier(MPI_COMM_WORLD);
+
         printf("Step %d: Process %d received value: %d.\n", i, rank, recv_val);
         // update sum and curr_val 
         printf("Step %d: Process %d is adding %d to sum %d \n", i, rank, recv_val, sum_val);
         sum_val += recv_val;
         curr_val = recv_val; 
+        MPI_Barrier(MPI_COMM_WORLD);
         
-    }           
+    }       
+    MPI_Barrier(MPI_COMM_WORLD);    
 
     printf("final sum is %d on processor %d \n", sum_val, rank);
+    tstop = MPI_Wtime();
+    double time = tstop - tstart;
+    printf("Processor %d takes %f seconds \n", rank, time);
 
     MPI_Finalize();
 
